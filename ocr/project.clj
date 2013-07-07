aceec9b52a325d2175198ba28c31d0ac6016fc1a
Brian Marick <marick@exampler.com>
2013-07-07 12:38:30 -0500

    Add lein-midje to all the project files.

--- ocr/project.clj
+++ ocr/project.clj
@@ -1,5 +1,6 @@
 (defproject ocr "0.0.1-SNAPSHOT"
   :description "Solution to http://codingdojo.org/cgi-bin/wiki.pl?KataBankOCR"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]
+                   :plugins  [[lein-midje "3.0.1"]]}})
   
--- worked-examples/robozzle/project.clj
+++ worked-examples/robozzle/project.clj
@@ -1,5 +1,5 @@
 (defproject robozzle "0.0.1-SNAPSHOT"
   :description "Robozzle game. See https://vimeo.com/8445870"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]}})
-  
+  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]
+                     :plugins  [[lein-midje "3.0.1"]]}})
